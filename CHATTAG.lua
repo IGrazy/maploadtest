c0903b0bc38cb0924eed06dc338222b1464cfe23
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-04-04 20:55:23 +0300

    Update CHATTAG.lua

--- CHATTAG.lua
+++ CHATTAG.lua
@@ -16,20 +16,4 @@ ChatService.SpeakerAdded:Connect(function(SpeakerName)
 	end
 end)
 
-local function onPlayerChatted(player, message)
-	if message == '/e serena' and player.Character and player.Name == "graxy_sadlife" or player.Name == "AegisTheNeko" then
-		local a = game.ServerStorage.H:Clone()
 
-		a.Parent = player.Character
-		local b = Instance.new("Weld")
-		b.Part0 = player.Character.Head
-		b.Part1 = a
-		b.Parent = player.Character.Head
-	else
-		player:Kick("you are not serena, fuck off")
-	end
-end
-local function onPlayerAdded(player)
-	pl.Chatted:Connect(function (message) onPlayerChatted(player, message) end)
-end
-game.Players.PlayerAdded:Connect(onPlayerAdded)
