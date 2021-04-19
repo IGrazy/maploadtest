7b46240357e2fad8718f4b749f69a665d3c16d7a
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-04-19 17:54:49 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -22,97 +22,97 @@ remote.OnServerEvent:connect(function(Player)
 	name.Text = game.ReplicatedStorage.hideIt.PlayerList:FindFirstChild(Player.Name).Value
 	role.Text = ""
 
-	if name.Text == "Dev_Rigzzy" then
+	if Player.Name == "Dev_Rigzzy" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.901961, 0.619608, 0.0509804)
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.870588, 0.298039, 0.603922)
 		role.Text = "🌴 Rigs 🌴"
-	elseif name.Text == "graxy_sadlife" then
+	elseif Player.Name == "graxy_sadlife" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(1, 0.580392, 0.0666667)
 		name.Text = "Graxy"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.819608, 0.145098, 0.729412)
 		role.Text = "🌟 Creator - Dev 🌟" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "MeguminSuba" then
+	elseif Player.Name == "MeguminSuba" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.00392157, 0.00227605, 0.000261438)
 		name.Text = "Megu"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.647059, 0.321569, 0.901961)
 		role.Text = "Professional Nekopara Player" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "Vuthanthanh1376" then
+	elseif Player.Name == "Vuthanthanh1376" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.219608, 0.635294, 1)
 		name.Text = "Louis"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.819608, 0.145098, 0.729412)
 		role.Text = "⭐Diviner⭐" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "Ygssefa" then
+	elseif Player.Name == "Ygssefa" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.317647, 0.67451, 0.745098)
 		name.Text = "Rape Man"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.121569, 0.282353, 0.494118)
 		role.Text = "I don't care if you have a boyfriend, bend over" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "CTHgamings" or name.Text == "miki_gold" then
+	elseif Player.Name == "CTHgamings" or name.Text == "miki_gold" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.988235, 0.0705882, 0.270588)
 		name.Text = "Miki"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.988235, 0.0705882, 0.270588)
 		role.Text = "Mapper/Developer" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "Mina_Joinson" then
+	elseif Player.Name == "Mina_Joinson" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.988235, 0.141176, 0.917647)
 		name.Text = "Snow"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.462745, 0.2, 0.988235)
 		role.Text = "Animation Maker" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "X_lillyuvu1" then
+	elseif Player.Name == "X_lillyuvu1" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new( 0.92549, 0.121569, 0.847059)
 		name.Text = "Dachi"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.92549, 0.121569, 0.847059)
 		role.Text = "Nice pants, Can I test the zipper?" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "LxstGxbrieI" then
+	elseif Player.Name == "LxstGxbrieI" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.0117647, 0, 0.00784314)
 		name.Text = "Gabe"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.0117647, 0, 0.00784314)
 		role.Text = "Your new daddy" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "AegisTheNeko" then
+	elseif Player.Name == "AegisTheNeko" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.941177, 0.0941177, 0.901961)
 		name.Text = "Serena"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.941177, 0.0941177, 0.901961)
 		role.Text = "Serena Thunder legs" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "CeaselessShawnjj" then
+	elseif Player.Name == "CeaselessShawnjj" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.00784314, 0.686275, 1)
 		name.Text = "SlurpingNeko"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(1, 1, 1)
 		role.Text = "Slurping on noodles" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "vNevaa" then
+	elseif Player.Name == "vNevaa" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.321569, 1, 0.219608)
 		name.Text = "Nevaa"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0.321569, 1, 0.219608)
 		role.Text = "Love pillar" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "altskipping" then
+	elseif Player.Name == "altskipping" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.819608, 0.145098, 0.729412)
 		name.Text = "alty"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(1, 1, 1)
 		role.Text = "Slurping that water" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "ppisbroken" then
+	elseif Player.Name == "ppisbroken" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0, 0.333333, 1)
 		name.TextColor3 = Color3.new(0.0235294, 0.0235294, 0.0235294)
@@ -121,7 +121,7 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextStrokeColor3 = Color3.new(0.219608, 0.623529, 1)
 		role.TextColor3 = Color3.new(0.0235294, 0.0235294, 0.0235294)
 		role.Text = "Ducks are cool" -- add ur rank what u want it 2 be called [by rigs]
-	elseif name.Text == "blackrzoses" then
+	elseif Player.Name == "blackrzoses" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(1, 0.0392157, 0.984314)
 		name.Text = "Lola"
@@ -130,7 +130,7 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextStrokeColor3 = Color3.new(0.027451, 0.027451, 0.027451)
 		role.Text = "Killing Ro-Gangsters since 2005" -- add ur rank what u want it 2 be called [by rigs]
 		role.Font = Enum.Font.JosefinSans
-	elseif name.Text == "luxmp3W" then
+	elseif Player.Name == "luxmp3W" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(1, 1, 0)
 		name.TextColor3 = Color3.new(0.0509804, 0.0509804, 0.0509804)
@@ -141,7 +141,7 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextColor3 = Color3.new(0.0509804, 0.0509804, 0.0509804)
 		role.Text = "Requiem" -- add ur rank what u want it 2 be called [by rigs]
 		role.Font = Enum.Font.JosefinSans
-	elseif name.Text == "VARl0US" then
+	elseif Player.Name == "VARl0US" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0.380392, 0.00392157, 0.705882)
 		name.TextColor3 = Color3.new(1,1,1)
