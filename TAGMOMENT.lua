d2fd888791b04ffd0768a2862b45cb3e6c192b5b
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-06-11 00:06:13 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -369,5 +369,16 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextColor3 = Color3.new(0,0,0)
 		role.Text = "" -- add ur rank what u want it 2 be called [by rigs]
 		role.Font = Enum.Font.JosefinSans
+	elseif Player.Name == "KommyLovesSatan" then
+		name.TextStrokeTransparency = 0
+		name.TextStrokeColor3 = Color3.new(0.580392, 0.388235, 0.580392)
+		name.TextColor3 = Color3.new(1, 0.768627, 0.929412)
+		name.Text = "Kom"
+		name.Font = Enum.Font.JosefinSans
+		role.TextStrokeTransparency = 0
+		role.TextStrokeColor3 = Color3.new(0.580392, 0.388235, 0.580392)
+		role.TextColor3 = Color3.new(1, 0.768627, 0.929412)
+		role.Text = "The Silly Kitty" -- add ur rank what u want it 2 be called [by rigs]
+		role.Font = Enum.Font.JosefinSans
 	end
 end)
