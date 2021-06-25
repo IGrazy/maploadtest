b63dae4fc42e1515b34686b436dacf9640e7c6e6
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-06-26 02:09:24 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -451,7 +451,7 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextStrokeColor3 = Color3.new(0,0,0)
 		role.TextColor3 = Color3.new(1,1,1)
 		role.Text = 'King Of Solan | Mapper' -- add ur rank what u want it 2 be called [by rigs]
-	elseif Player.Name == "CLUBKLD" then
+	elseif Player.Name == "CLUBKlD" then
 		name.TextStrokeTransparency = 1
 		name.TextStrokeColor3 = Color3.new(1,1,1)
 		name.TextColor3 = Color3.new(1,1,0)
