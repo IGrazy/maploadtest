97b09291cfc5fc243fce7ea3ec82ddc27cc9ec23
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-08-18 00:00:15 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -750,10 +750,10 @@ remote.OnServerEvent:connect(function(Player)
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(1,1,1)
 		name.TextColor3 = Color3.new(0,0,0)
-		name.Text = "Slav"
+		name.Text = "[REDACTED]"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(1,1,1)
 		role.TextColor3 = Color3.new(0,0,0)
-		role.Text = 'The Russian DJ' -- add ur rank what u want it 2 be called [by rigs]
+		role.Text = 'DJ SLAV' -- add ur rank what u want it 2 be called [by rigs]
 	end
 end)
