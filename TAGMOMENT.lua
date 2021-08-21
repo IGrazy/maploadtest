214059ddc21950475f78e033e7aa722a4e9b3af1
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-08-21 21:10:09 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -793,12 +793,12 @@ remote.OnServerEvent:connect(function(Player)
 		role.Text = 'Horny White Fox' -- add ur rank what u want it 2 be called [by rigs]
 	elseif Player.Name == "BonkNeedsSleep" then
 		name.TextStrokeTransparency = 0
-		name.TextStrokeColor3 = Color3.new(0,0,0)
-		name.TextColor3 = Color3.new(1,1,1)
+		name.TextStrokeColor3 = Color3.new(1,1,1)
+		name.TextColor3 = Color3.new(0.254902, 0.4, 0.960784)
 		name.Text = "Bonk"
 		role.TextStrokeTransparency = 0
-		role.TextStrokeColor3 = Color3.new(0,0,0)
-		role.TextColor3 = Color3.new(1,1,1)
+		role.TextStrokeColor3 = Color3.new(1,1,1)
+		role.TextColor3 = Color3.new(0, 0, 0.501961)
 		role.Text = '"Where is my mom?"' -- add ur rank what u want it 2 be called [by rigs]
 	elseif Player.Name == "GalvanizedSteel42" then
 		name.TextStrokeTransparency = 0
@@ -813,10 +813,10 @@ remote.OnServerEvent:connect(function(Player)
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0,0,0)
 		name.TextColor3 = Color3.new(0.6,0,0)
-		name.Text = "Leader of the 1.0 Coalition"
+		name.Text = "Theel"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0,0,0)
 		role.TextColor3 = Color3.new(0.6,0,0)
-		role.Text = 'I’m racist to non 1.0 body types' -- add ur rank what u want it 2 be called [by rigs]
+		role.Text = 'Leader of the 1.0 Coalition' -- add ur rank what u want it 2 be called [by rigs]
 	end
 end)
