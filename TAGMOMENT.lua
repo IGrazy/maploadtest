199638f91974cfc4910f4f87fe91d7049a62f6aa
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-08-22 17:54:27 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -821,11 +821,11 @@ remote.OnServerEvent:connect(function(Player)
 	elseif Player.Name == "JormungandKnows" then
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(0,0,0)
-		name.TextColor3 = Color3.new(0.6,0,0)
+		name.TextColor3 = Color3.new(1,1,1)
 		name.Text = "Jörmungandr"
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(0,0,0)
-		role.TextColor3 = Color3.new(0.6,0,0)
+		role.TextColor3 = Color3.new(1,1,1)
 		role.Text = 'Your local Russian' -- add ur rank what u want it 2 be called [by rigs]
 	end
 end)
