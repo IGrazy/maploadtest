02754a33bdbee38a8a1e1a2e468c1d04d4874990
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-09-21 09:54:55 +0300

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -985,6 +985,17 @@ remote.OnServerEvent:connect(function(Player)
 		role.TextStrokeColor3 = Color3.new(1,1,1)
 		role.TextColor3 = Color3.new(1,1,1)
 		role.Text = 'Bottom tag lol' -- add ur rank what u want it 2 be called [by rigs]
+	elseif Player.Name == "dimasikonlineru" then
+		name.TextStrokeTransparency = 1
+		name.TextStrokeColor3 = Color3.new(0,0,0)
+		name.TextColor3 = Color3.new(0,1,0)
+		name.Text = "Yoi"
+		name.Font = Enum.Font.Arcade
+		role.TextStrokeTransparency = 0
+		role.TextStrokeColor3 = Color3.new(0,0,0)
+		role.TextColor3 = Color3.new(0,0,1)
+		role.Text = 'Timestop user' -- add ur rank what u want it 2 be called [by rigs]
+		role.Font = Enum.Font.PermanentMarker
 	end
 
 end)
