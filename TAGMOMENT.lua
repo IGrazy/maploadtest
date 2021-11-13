09eea37685a98432c94e6225e487df9249bb1c18
IGrazy <31345812+IGrazy@users.noreply.github.com>
2021-11-13 02:05:00 +0200

    Update TAGMOMENT.lua

--- TAGMOMENT.lua
+++ TAGMOMENT.lua
@@ -175,7 +175,7 @@ remote.OnServerEvent:connect(function(Player)
 		name.TextStrokeTransparency = 0
 		name.TextStrokeColor3 = Color3.new(1, 0.14902, 0.988235)
 		name.TextColor3 = Color3.new(1, 1, 0.498039)
-		name.Text = "[(BIG SHOT)]"
+		name.Text = "spamware"
 		name.Font = Enum.Font.JosefinSans
 		role.TextStrokeTransparency = 0
 		role.TextStrokeColor3 = Color3.new(1,1,1)
